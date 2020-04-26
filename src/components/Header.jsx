7e398bd8e4c57713ed21b5aacb0a4b34651e95d9
chefthomas <thomcdixon@gmail.com>
2020-04-26 13:47:46 -0700

    fix #1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 
+import "../stylesheets/css/main.css";
+
 const Header = (props) => (
   <header className='Header'>
     <Logo />
