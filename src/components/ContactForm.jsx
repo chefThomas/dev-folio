bbe5e4a1d264d8e979e4ec5e3fa2c1f21e04f4b5
chefthomas <thomcdixon@gmail.com>
2020-05-16 13:47:12 -0700

    began contact form before checkinb out

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <form className='ContactForm' action=''>
+      <div className='ContactForm__info'></div>
+      <div className='ContactForm__message'></div>
+    </form>
+  );
+};
+
+export default ContactForm;
--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "./Footer";
 import Heading from "./Heading";
 import Page from "./Page";
+import ContactForm from "./ContactForm";
 
 const ContactPage = () => {
   return (
     <Page className='Page'>
       <Heading level={2} text='Contact' />
+      <ContactForm />
       <Footer />
     </Page>
   );
