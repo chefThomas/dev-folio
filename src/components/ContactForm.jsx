898023de5c97f7a58c1b8ed19b3a48c5da8814b5
chefthomas <thomcdixon@gmail.com>
2020-05-16 21:48:49 -0700

    almost finished with contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Styles from "../mui-style-override/styles";
+
+import theme from "../mui-style-override/theme";
+import { ThemeProvider } from "@material-ui/styles";
+
+import emailValidator from "../helpers/emailValidator";
 
 const ContactForm = () => {
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  const handleSubmit = () => {
+    // reset error state
+    setEmailError(false);
+
+    // validate email
+    const emailValid = emailValidator.test(email);
+    // validate password
+
+    //validate passworids match
+
+    if (emailValid) {
+      console.log("valid entries");
+      // POST to new user endpoint
+    } else {
+      if (!emailValid) {
+        setEmailError(true);
+      }
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleCompanyChange = (e) => {
+    setCompany(e.target.value);
+  };
+
   return (
-    <form className='ContactForm' action=''>
-      <div className='ContactForm__info'></div>
-      <div className='ContactForm__message'></div>
+    // <div className='ContactForm'>
+    <form className='ContactForm'>
+      <div className='ContactForm__col1'>
+        <ThemeProvider theme={theme}>
+          <TextField
+            style={Styles.form.textfield}
+            size='small'
+            value={name}
+            onChange={handleNameChange}
+            variant='outlined'
+            label='Name'
+            // color='primary'
+            error={emailError || null}
+            helperText={emailError ? "Invalid email" : ""}
+          />
+          <TextField
+            style={Styles.form.textfield}
+            size='small'
+            value={company}
+            onChange={handleCompanyChange}
+            variant='outlined'
+            label='Company'
+          />
+          <TextField
+            style={Styles.form.textfield}
+            size='small'
+            value={email}
+            onChange={handleEmailChange}
+            variant='outlined'
+            label='Email'
+            color='primary'
+            error={emailError || null}
+            helperText={emailError ? "Invalid email" : ""}
+          />
+        </ThemeProvider>
+        <Button
+          style={{ ...Styles.button.primary, marginBottom: "2rem" }}
+          onClick={handleSubmit}
+        >
+          SEND
+        </Button>
+      </div>
+      <div className='ContactForm__col2'>
+        <TextField
+          variant='outlined'
+          size='small'
+          style={{ ...Styles.form.textfield, width: "100%" }}
+          value={message}
+          label='Message'
+          color='primary'
+          error={messageError || null}
+          helperText={messageError ? "Don't be shy" : ""}
+          multiline
+          rows={9}
+        />
+      </div>
     </form>
+    // </div>
   );
 };
 
---
+++ src/mui-style-override/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const customTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00a4cb",
+    },
+  },
+  spacing: 8,
+});
+
+export default customTheme;
